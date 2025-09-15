6cece5e9ded10f41896d6954fe933cbcd120bdf6
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-15 20:08:02 +0530

    first commit

---
+++ basis.js
@@ -0,0 +1,31 @@
+// Initially created to make our page alive
+// It is high level programming scripting , interpreted and single threaded 
+// Developed by brendon eich 1995. company netscape communications. developed in 10 days. first name is mocha.
+// Features:
+//  It has full integration with HTML and css
+//  Simple things are done simply
+//  It exceutes directly inside the browser when page loaded everytime
+//  Doesnt required any special preparation and complation for exceution
+//  All major browser supports JS
+//  It is case sensitive
+// .js - extension 
+
+// Writing and exceuting 
+// 1. Directly inside the browser, using console
+// 2. in html doc using script we can embed
+// 3. by using run time environment like nodejs
+
+console.log("hello adarsh madhusoodhanan");
+
+// window.alert("Hello adarsh dont laugh at me ");
+
+// document.getElementById('my').textContent='Hello';
+
+// assiging variables:
+let first_name="Adarsh";
+let age=22;
+
+console.log(first_name,age)
+
+console.log(typeof first_name);
+console.log(typeof age)
