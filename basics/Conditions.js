bf3d1b4bf61d582963eb6313628b5c42f1b9891f
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-11-01 13:15:32 +0530

    Conditions

--- basics/Conditions.js
+++ basics/Conditions.js
@@ -1,98 +1,39 @@
-// Expression:
-//      combination of operants and operator
-//      peace of code which produces some value
-
-// Types:
-//      Arthmetic: +,-,/,*,%,**,++,--
-//      Assignment: =,+=,-=,*=,/=,%=,**=
-//      Comparision: ==,!=,===,!==,<,>,<=,>=,?
-//      Logical: AND(&&), OR(||), NOT(!)
-
-// Arthmetic
-
-// increment(++)- preincrement(++a): first value increments later prints
-//                postincrement(a++): first prints value later increments 
-
-// decrement(--)- predecrement(++a): first value decrements later prints
-//                postdecrement(a++): first prints value later decrements 
-
-let x=99
-console.log(x--)                //99
-console.log(x++)                //98
-console.log(++x)                //100
-console.log(x)                  //100
-console.log(--x)                //99
-console.log(x)                  //99
-
-// --------------------------------------------------------------------------------------------
-
-// assignment
-
-let a=10
-a+=5
-console.log(a)                  //15
-a-=5
-console.log(a)                  //10
-a*=2
-console.log(a)                  //20
-a/=2
-console.log(a)                  //10
-a**=2
-console.log(a)                  //100
-
-// -------------------------------------------------------------------------------------------
-
-// comparision
-
-let p=10
-let q="10"
-
-console.log("p==q:",p==q)                   //p==q: true
-console.log("p===q:",p===q)                 //p===q: false
-
-let result = p == q ? "p & q are equal": "p & q are not equal"
-console.log(result)                         // p & q are equal
-
-// --------------------------------------------------------------------------------------------
-
-// Logical 
-
-//     &&             ||            NOT-!          !response.OR
-//  T | T | T      T | T | T       T | F
-//  T | F | F      T | F | T       F | T
-//  F | T | F      F | T | T
-//  F | F | F      F | F | F 
-
 // Conditions
 
 // Controlling the flow of program by using certain set of rules
 // Types:
-//      1. if
+//      1.if
 //      2.if-else
 //      3.else-if
 //      4.nested if
 //      5.Switch case
 
+// ------------------------------------------------------------------------------------------
+
 // if Statement:
 
-const prompt = require("prompt-sync")({sigint:true}) 
-// let userName = prompt("enter the name:");
-let userName="Keerthana"
+const prompt = require("prompt-sync")({sigint:true}) //signal interrupt
+let userName = prompt("enter the name:");
+// let userName="Keerthana"
 if(userName==="Keerthana"){
     console.log("user found")
 }
 // output: user found
 
-// if-else
+// ---------------------------------------------------------------------------------
+
+// if-else :
 
 if(userName==="Adarsh"){
-    console.log("user found")               //user found
+    console.log("user found")              
 }
 else{
     console.log("user not found")           //user not found
 }
 
-// else-if
+// -----------------------------------------------------------------------------------
+
+// else-if :
 
 let speed= prompt("enter the speed")
 if (speed<=40){
@@ -107,3 +48,79 @@ else if(speed>100 && speed<200){
 else{
     console.log("Welcome too heaven")
 }
+
+// ------------------------------------------------------------------------------------------------------
+
+// Nested-if :
+
+let profileName = prompt("enter the name:");
+if(profileName==="Adarsh"){
+    let password=prompt("enter the password")
+    if(password==="keerhi@123") {
+        console.log(`Profile ${profileName}`)
+        console.log("username",profileName)
+        console.log("password",password)
+    }  
+    else{
+        console.log("incorrect password")
+    }          
+}
+else{
+    console.log("user not found")           //user not found
+}
+
+// ------------------------------------------------------------------------------------------------------
+
+let metroStatus=prompt("enter the metro status True or False:")==="true";
+
+if (metroStatus){
+    let passangerExist=prompt("enter the passanger status True or False:")==="true";
+    if(passangerExist){
+        console.log("Journey ended")
+    }
+    else{
+        console.log("passanger still inside")
+    }
+}
+else{
+    console.log("Paasanger not entered")
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// switch case
+
+let day = new Date().getDay()
+// console.log(day)  ->4 
+let today
+switch(day){
+    case 0:
+        today="Sunday"
+        break
+    case 1:
+        today="Monday"
+        break
+    case 2:
+        today="Tuesday"
+        break
+    case 3:
+        today="Wednesday"
+        break
+    case 4:
+        today="Thursday"
+        break
+    case 5:
+        today="Friday"
+        break
+    case 6:
+        today="Saturday"
+        break
+    default:
+        console.log("invalid")
+}
+console.log(`today is ${today}`)                 // today is Thursday
+
+// -----------------------------------------------------------------------------------------------------
+
+week=['Sunday','Monday','Tuesday','Wednesday','Thursaday','Friday','Saturday']
+console.log(`today is ${week[day]}`)             // today is Thursday
